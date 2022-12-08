ebba11c6782f20def6c08f76e23e2094f67f693e
Justin Dang <dangjustin95@gmail.com>
2022-12-08 08:43:47 -0600

    userController validation and bcrypt

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler')
-const Goal = require('../models/goalmodel')
+const Goal = require('../models/goalModel')
 
 //@desc Get Goals
 //@route GET /api/goals
